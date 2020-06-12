5340e98a098e63d69c7fca33d66b2ec16eb94461
Nick Perez <nick@Nicks-MacBook-Pro.local>
2020-06-11 23:22:38 -0700

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,48 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+const newItem = {
+    name: item.name,
+    enhancement: (item.enhancement < 20 ? item.enhancement+=1 : item.enhancement = 20),
+    durability: item.durability
+  }
+  return newItem;
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15){
+    const newItem = {
+      name: item.name,
+      enhancement: item.enhancement,
+      durability: item.durability - 5
+  }
+  return newItem
+} else if (item.enhancement > 16) {
+const newItem = {
+  name: item.name,
+  enhancement: item.enhancement - 1,
+  durability: item.durability - 10
+}
+return newItem
+} else if (item.enhancement > 14) {
+  const newItem = {
+    name: item.name,
+    enhancement: item.enhancement,
+    durability: item.durability - 10
+}
+ return newItem;
+
+}
+
 }
 
 function repair(item) {
-  return { ...item };
+	 const newItem = {
+    name: item.name,
+    enhancement: item.enhancement,
+    durability: 100
+  }
+  return newItem;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,116 @@
-const enhancer = require('./enhancer.js');
+// const enhancer = require('./enhancer.js');
+const {repair, succeed, fail}  = require('./enhancer.js');
+
 // test away!
+describe("enhancer.js", function() {
+    describe("repair ", function() {
+        it('should return 100', function() {
+            const item = { 
+                name:"Test",
+                enhancement: 20,
+                durability:3 
+            };
+            const expected = {
+                name:"Test",
+                enhancement: 20,
+                durability:100
+            };
+
+            expect(repair(item)).toEqual(expected);
+        })
+    })
+     describe("succeed", () => {
+        it('enhancement increases by 1', () => {
+            const item = {
+                name: "Test",
+                enhancement: 1,
+                durability: 100
+            }
+
+            const expected = {
+                name: "Test",
+                enhancement: 2,
+                durability: 100
+            }
+
+            expect(succeed(item)).toEqual(expected);
+        })  
+        it('enhancement max at 20 ', () => {
+            const item = {
+                name: "Test",
+                enhancement: 20,
+                durability: 100
+            }
+
+            const expected = {
+                name: "Test",
+                enhancement: 20 ,
+                durability: 100
+            }
+
+            expect(succeed(item)).toEqual(expected);
+        })    
+         it('durability is unchanged ', () => {
+            const item = {
+                name: "Test",
+                enhancement: 20,
+                durability: 100
+            }
+
+            const expected = {
+                name: "Test",
+                enhancement: 20 ,
+                durability: 100
+            }
+
+            expect(succeed(item)).toEqual(expected);
+        })
+    })
+     describe("fails", () => {
+        it(" durability decreased by 5 If the item's enhancement is less than 15", () => {
+            const item = {
+                name: "Test",
+                enhancement: 10,
+                durability: 100
+            }
+
+            const expected = {
+                name: "Test",
+                enhancement: 10,
+                durability: 95
+            }
+
+            expect(fail(item)).toEqual(expected);
+        })   
+           it(" durability is decreased by 10, If the item's enhancement is 15 or more", () => {
+            const item = {
+                name: "Test",
+                enhancement: 15,
+                durability: 100
+            }
+
+            const expected = {
+                name: "Test",
+                enhancement: 15,
+                durability: 90
+            }
+
+            expect(fail(item)).toEqual(expected);
+        })   
+           it("  the item's enhancement level is greater than 16, the enhancement level decreases by 1", () => {
+            const item = {
+                name: "Test",
+                enhancement: 18,
+                durability: 100
+            }
+
+            const expected = {
+                name: "Test",
+                enhancement: 17,
+                durability: 90
+            }
+
+            expect(fail(item)).toEqual(expected);
+        })  
+    })  
+})
